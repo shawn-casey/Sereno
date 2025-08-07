45b7f04cba29cb0703044419f7cd8f248ac37566
Shawn <Shawn>
2025-08-06 23:20:33 -0400

    Initial Commit

---
+++ Sereno/SerenoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SerenoApp.swift
+//  Sereno
+//
+//  Created by Shawn Casey on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SerenoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
